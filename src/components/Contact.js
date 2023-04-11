4dd0ec5aec82bae2abdcafcc2646af36d854d7ff
shivacheripally <125255635+shivacheripally@users.noreply.github.com>
2023-04-11 22:28:34 +0530

    1 file modified component error is modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-function Stuff() {
-  <div>
-    <h2>GOT QUESTIONS?</h2>
-    <p>
-      The easiest thing to do is post on our{' '}
-      <a href="http://forum.kirupa.com">forums</a>.
-    </p>
-  </div>;
+function Contact() {
+  return (
+    <div>
+      <h2>GOT QUESTIONS?</h2>
+      <p>
+        The easiest thing to do is post on our{' '}
+        <a href="http://forum.kirupa.com">forums</a>.
+      </p>
+    </div>
+  );
 }
 
-export default Stuff;
+export default Contact;
