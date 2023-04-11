b61b384d96bd9ee44b4ddb8f746d12afb43da5d8
shivacheripally <125255635+shivacheripally@users.noreply.github.com>
2023-04-11 21:24:13 +0530

    components folder created

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Simple SPA</h1>
+      <ul className="list">
+        <li><a href="">Home</a></li>
+        <li><a href="">Stuff</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+  );
+}
