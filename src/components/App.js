83d215549d40f97a9f25eaa88c5316dc02b4de61
shivacheripally <125255635+shivacheripally@users.noreply.github.com>
2023-04-11 22:10:29 +0530

    3 files modified and used react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
+import { BrowserRouter ,Routes, Route, NavLink } from 'react-router-dom';
+import { Home, Stuff, Contact } from './index.js';
 
 export default function App() {
   return (
-    <div>
-      <h1>Simple SPA</h1>
-      <ul className="list">
-        <li><a href="">Home</a></li>
-        <li><a href="">Stuff</a></li>
-        <li><a href="">Contact</a></li>
-      </ul>
+    <BrowserRouter>
+      <div>
+        <h1>Simple SPA</h1>
+        <ul className="list">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/stuff">Stuff</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
 
-      <div className="content">
-        
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={Home} />
+            <Route exact path="/stuff" element={Stuff} />
+            <Route exact path="/contact" element={Contact} />
+          </Routes>
+        </div>
       </div>
-    </div>
+      </BrowserRouter>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-import App from './App,js';
-import Home from './Home,js';
-import Stuff from './Stuff,js';
-import Contact from './Contact,js';
+import App from './App.js';
+import Home from './Home.js';
+import Stuff from './Stuff.js';
+import Contact from './Contact.js';
 
 export {App,Home,Stuff,Contact};
